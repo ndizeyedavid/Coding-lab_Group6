57fa4fd9d14a512a1a577c856c3d846e5f36773c
eruhigira-tech <e.ruhigira@alustudent.com>
2026-05-30 19:37:13 +0200

    Initialize system directories if they do not exist
    Added checks and creation for active_logs, archived_logs, and reports directories in initialize_system function.

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 initialize_system() {
-# Member 1's function
+    if [ -d "active_logs" ]; then
+        echo "active_logs directory already exists."
+    else
+        echo "Creating active_logs directory..."
+        mkdir active_logs
+
+    fi
+
+    if [ -d "archived_logs" ]; then
+        echo "archived_logs directory already exists."
+    else
+        echo "Creating archived_logs directory..."
+        mkdir archived_logs
+    fi
+
+    if [ -d "reports" ]; then
+        echo "reports directory already exists."
+    else
+        echo "Creating reports directory..."
+        mkdir reports
+    fi
+
+    echo "System directories initialized."
 }
 
 secure_data() {
